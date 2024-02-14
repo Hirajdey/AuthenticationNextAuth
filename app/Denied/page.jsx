768e095e5d02aa66191f67745661cf3e714fb424
HirajDey <deyhiraj@gmail.com>
2024-02-14 22:55:19 +0530

    feat: basic layout

---
+++ app/Denied/page.jsx
@@ -0,0 +1,7 @@
+const Denied = () => {
+  return (
+    <div><h1 className="text-red-400">Denied</h1></div>
+  )
+}
+
+export default Denied
---
+++ app/Member/page.jsx
@@ -0,0 +1,8 @@
+// Remember if you don't add 'use client' statement it's going to be render the page on server side   
+const Member = () => {
+  return (
+    <div><h1>Member Server Session</h1></div>
+  )
+}
+
+export default Member
